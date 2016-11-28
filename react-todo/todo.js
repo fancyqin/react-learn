6f544b1b9e267ade059578a019e53faf790c824c
Fan <fancarter007@163.com>
2016-11-28 17:32:35 +0800

    :octopus: :zap: :cat: = :octocat:

--- react-todo/todo.js
+++ react-todo/todo.js
@@ -3,30 +3,50 @@
  */
 
 var TodoBox = React.createClass({
-    bdKeyUp: function(){
-        if(this.refs.in.value){
-            //todo
+    //add
+    bdKeyUp: function(e){
+        var input = this.refs.inputIn;
+        if(input.value && e.which === 13){
+            var date = new Date();
+            var itemId = date.getTime();
+            var thisData = {id:itemId,text:input.value,done:false};
+            this.setState({data:this.state.data.concat([thisData])});
+            input.value ='';
         }
     },
     getInitialState: function(){
         return {
-            data:[{text:'qfinaf',time:"1989-01-30"},{text:'hfeyeety',time:"1993-02-16"}]
+            data:[
+                //{text:':octopus: :zap: :cat: = :octocat:',time:"1989-01-30",done:true},
+                //{text:'hfeyeety',time:"1993-02-16",done:false}
+            ]
         }
     },
     render: function(){
         return (
             <div className="todo-box">
                 <div className="todo-in">
-                    <input type="text" ref="in" onkeyup={this.bdKeyUp}/>
+                    <input type="text" ref="inputIn" placeholder="Please enter something.." onKeyUp={this.bdKeyUp}/>
                 </div>
                 <TodoList data={this.state.data} />
             </div>
         )
+    },
+    itemChange: {
+        changeItem: function(id){
+
+        }.bind(this),
+        deleteItem: function(id){
+            for (item in this.state.data){
+                if(item.id === id){
+                    
+                }
+            }
+        }.bind(this)
     }
 });
 var TodoList = React.createClass({
     render: function () {
-        console.log(11);
         return (
             <ul className="todo-list">
                 {
@@ -41,11 +61,24 @@ var TodoList = React.createClass({
     }
 });
 var TodoItem = React.createClass({
+    getInitialState: function () {
+         return {
+             done: this.props.data.done
+         }
+    },
+    bdDoneChange: function () {
+        this.setState({done:this.refs.checkbox.checked});
+        console.log(this.props.data);
+    },
+    bdDelete:function(){
+
+    },
     render: function () {
         return(
-            <li className="todo-item">
-                {this.props.data.text}
-                <span className="time">{this.props.data.time}</span>
+            <li className="todo-item" ref="item" id={this.props.data.id}>
+                <input type="checkbox" ref="checkbox" className="checkbox" onChange={this.bdDoneChange} checked={this.state.done} />
+                <input type="text" className="inner" onChange={this.todo} value={this.props.data.text}/>
+                <span className="close" onClick={this.bdDelete}>&times;</span>
             </li>
         )
     }
