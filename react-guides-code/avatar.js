26f8c9ff6553b1fee32fbb9c239293b7c902c88d
Fan <fancarter007@163.com>
2016-12-06 17:15:45 +0800

    :octocat:

--- react-guides-code/avatar.js
+++ react-guides-code/avatar.js
@@ -1,3 +1,36 @@
 /**
  * Created by qinfan on 2016/12/5.
  */
+var Avatar = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <ProfilePic username={this.props.username} />
+                <ProfileLink username={this.props.username} />
+            </div>
+        );
+    }
+});
+
+var ProfilePic = React.createClass({
+    render: function() {
+        return (
+            <img src={'http://graph.facebook.com/' + this.props.username + '/picture'} />
+        );
+    }
+});
+
+var ProfileLink = React.createClass({
+    render: function() {
+        return (
+            <a href={'http://www.facebook.com/' + this.props.username}>
+                {this.props.username}
+            </a>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Avatar username="pwh" />,
+    document.getElementById('avatar')
+);
